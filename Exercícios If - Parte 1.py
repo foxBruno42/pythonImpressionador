4aacd6330078a2050583d7edd8409aa61c163a5f
Fox Bruno <35773364+foxBruno42@users.noreply.github.com>
2024-01-11 23:17:02 -0300

    Novas alterações
    Finalização do exercicio de  ifs, descoberto as diversas formas de impressão do python e feito lista de lições de correção referente a sequencia estrutural

--- Exercícios If - Parte 1.py
+++ Exercícios If - Parte 1.py
@@ -23,10 +23,19 @@ vendas_funcionario2 = 770
 vendas_funcionario3 = 2700
 meta=1000
 if(vendas_funcionario1 >=meta):
-    print('O funcionário 1 ganhou ',(meta*0.10),'de bônus')
+    print('O funcionário 1 ganhou ',(vendas_funcionario1*0.10),' de bônus')
 else:
-    print ('oi')
+    print('O funcionário 1 ganhou 0 de bônus')
 
+if(vendas_funcionario2 >=meta):
+    print('O funcionário 2 ganhou {} de bônus',vendas_funcionario2*0.10)
+else:
+    print('O funcionário 2 ganhou 0 de bônus')
+
+if(vendas_funcionario3 >=meta):
+    print(f"O funcionário 3 ganhou {(vendas_funcionario3*0.10)} de bônus")
+else:
+    print('O funcionário 3 ganhou 0 de bônus')
 """Resposta:
 O funcionário 1 ganhou 100 de bônus
 O funcionário 2 ganhou 0 de bônus
@@ -45,8 +54,27 @@ Agora, os funcionários que venderem muito acima da meta ganham mais bônus do q
 
 Use as mesmas variáveis de vendas_funcionários
 """
+if(vendas_funcionario1 >=2000):
+    print('O funcionário 1 ganhou ',(vendas_funcionario1*0.15),' de bônus')
+elif(vendas_funcionario1 >=1000):
+    print('O funcionário 1 ganhou ',(vendas_funcionario1*0.10),' de bônus')
+else:
+    print('O funcionário 1 ganhou 0 de bônus')
 
-#crie seu código aqui
+if(vendas_funcionario2 >=2000):
+    print('O funcionário 2 ganhou {} de bônus',vendas_funcionario2*0.15)
+elif(vendas_funcionario2 >=1000):
+    print('O funcionário 2 ganhou {} de bônus',vendas_funcionario2*0.10)
+else:
+    print('O funcionário 2 ganhou 0 de bônus')
+
+if(vendas_funcionario3 >=2000):
+    print(f"O funcionário 3 ganhou {(vendas_funcionario3*0.15)} de bônus")
+elif(vendas_funcionario3 >=1000):
+    print(f"O funcionário 3 ganhou {(vendas_funcionario3*0.10)} de bônus")
+else:
+    print('O funcionário 3 ganhou 0 de bônus')
+
 #obs: há 2 formas de fazer, com if dentro de if ou então com if e elif. Escolha a que você preferir
 
 """Resposta:
